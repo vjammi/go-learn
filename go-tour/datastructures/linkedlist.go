7be1bb959536954b7bfbddfaa152f2c4279ac83b
Vijay Jammi <vjammi@gmail.com>
2022-09-11 02:33:20 -0400

    .

--- go-tour/datastructures/linkedlist.go
+++ go-tour/datastructures/linkedlist.go
@@ -1,4 +1,4 @@
-package datastructures
+package main
 
 import (
 	"fmt"
@@ -34,24 +34,29 @@ func (list *linkedList) Add(node *listNode) {
 	}
 }
 
-func (list *linkedList) add(val int) {
-	listNode := listNode{val: val}
+func (list *linkedList) addNode(val int) {
+	// listNode := &listNode{val: val}
+	// or
+	// listNode := new(listNode)
+	// listNode.val = val
+
+	listNode := new(listNode)
+	listNode.val = val
 
 	if list.head == nil {
-		list.head = &listNode
-		list.tail = &listNode
+		list.head = listNode
+		list.tail = listNode
 		list.length++
 	} else {
-		list.tail.next = &listNode
-		list.tail = &listNode
+		list.tail.next = listNode
+		list.tail = listNode
 		list.length++
 	}
-
 	return
 }
 
 func (list *linkedList) Show() {
-	currentNode := list.head
+	currentNode := &list.head
 	for currentNode != nil {
 		fmt.Printf("%v -> ", list.head.val)
 		list.head = list.head.next
@@ -59,24 +64,24 @@ func (list *linkedList) Show() {
 	fmt.Println()
 }
 
+// Note: When we have a mix of (list *linkedList)  &&  (list linkedList)
+// Go Runtime complains Struct linkedList has methods on both value and pointer receivers. Such usage is not recommended by the Go Documentation.
 func (list *linkedList) show() {
 	current := list.head
-
 	for current != nil {
 		fmt.Println(current.val)
 		current = current.next
 	}
-
 }
 
-func (list linkedList) ShowFront() (int, error) {
+func (list *linkedList) ShowFront() (int, error) {
 	if list.head == nil {
 		return 0, fmt.Errorf("Cannot Find ShowFront Value in an Empty linked list")
 	}
 	return list.head.val, nil
 }
 
-func (list linkedList) ShowBack() (int, error) {
+func (list *linkedList) ShowBack() (int, error) {
 	if list.head == nil {
 		return 0, fmt.Errorf("Cannot Find ShowFront Value in an Empty linked list")
 	}
@@ -120,31 +125,44 @@ func (list *linkedList) Delete(key int) {
 
 }
 
-func LinkedListTest3() {
+func main() {
 
 	list := linkedList{}
-	node1 := &listNode{val: 20}
-	node2 := &listNode{val: 30}
-	node3 := &listNode{val: 40}
-	node4 := &listNode{val: 50}
-	node5 := &listNode{val: 70}
+	//node1 := &listNode{val: 20}
+	//node2 := &listNode{val: 30}
+	//node3 := &listNode{val: 40}
+	//node4 := &listNode{val: 50}
+	//node5 := &listNode{val: 70}
+	//
+	//list.Add(node1)
+	//list.Add(node2)
+	//list.Add(node3)
+	//list.Add(node4)
+	//list.Add(node5)
 
-	list.Add(node1)
-	list.Add(node2)
-	list.Add(node3)
-	list.Add(node4)
-	list.Add(node5)
+	list.addNode(10)
+	fmt.Println("Length = ", list.tail.val)
+	list.addNode(20)
+	fmt.Println("Length = ", list.tail.val)
+	list.addNode(30)
+	fmt.Println("Length = ", list.tail.val)
+	list.addNode(40)
+	fmt.Println("Length = ", list.tail.val)
+	list.addNode(50)
+	fmt.Println("Length = ", list.tail.val)
+
+	//list.Show()
+	list.show()
 	fmt.Println("Length = ", list.Len())
 
-	list.Show()
-	list.Delete(40)
-	list.Reverse()
-	fmt.Println("Length = ", list.Len())
-	list.Show()
-
-	front, _ := list.ShowFront()
-	back, _ := list.ShowBack()
-	fmt.Println("ShowFront = ", front)
-	fmt.Println("ShowBack = ", back)
+	//list.Delete(40)
+	//list.Reverse()
+	//fmt.Println("Length = ", list.Len())
+	//list.Show()
+	//
+	//front, _ := list.ShowFront()
+	//back, _ := list.ShowBack()
+	//fmt.Println("ShowFront = ", front)
+	//fmt.Println("ShowBack = ", back)
 
 }
