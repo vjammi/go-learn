b7d1707da4961c77571e6c04dd654012a726c7e6
Vijay Jammi <vjammi@gmail.com>
2022-09-11 22:59:44 -0400

    .

--- go-tour/datastructures/linkedlist.go
+++ go-tour/datastructures/linkedlist.go
@@ -9,39 +9,25 @@ type listNode struct {
 	next *listNode
 }
 
-// LinkedList is a linked list
 type linkedList struct {
 	head   *listNode
 	tail   *listNode
 	length int
 }
 
-// Len Function returns Length of the LinkedList
-func (list *linkedList) Len() int {
+func (list *linkedList) len() int {
 	return list.length
 }
 
-// Add Function inserts a new listNode at the end of the LinkedList
-func (list *linkedList) Add(node *listNode) {
-	if list.head == nil {
-		list.head = node
-		list.tail = node
-		list.length++
-	} else {
-		list.tail.next = node
-		list.tail = node
-		list.length++
-	}
-}
-
 func (list *linkedList) addNode(val int) {
-	//node := &listNode{val: val}
-	//		or
-	//node := new(listNode)
-	//node.val = val
-	//		which is same as
-	//var node *listNode = new(listNode)
-	node := new(listNode)
+	// node := &listNode{val: val} 				// list.head = &node
+	//	 or
+	// node := new(listNode)       				// list.head = node
+	// node.val = val
+	//   is same as
+	// var node *listNode = new(listNode)		// list.head = node
+
+	node := new(listNode) // var node *listNode = new(listNode)  // list.head = node
 	node.val = val
 
 	if list.head == nil {
@@ -56,114 +42,103 @@ func (list *linkedList) addNode(val int) {
 	return
 }
 
-func (list *linkedList) Show() {
-	currentNode := &list.head
-	for currentNode != nil {
-		fmt.Printf("%v -> ", list.head.val)
-		list.head = list.head.next
-	}
-	fmt.Println()
-}
-
 // Note: When we have a mix of (list *linkedList)  &&  (list linkedList)
 // Go Runtime complains Struct linkedList has methods on both value and pointer receivers. Such usage is not recommended by the Go Documentation.
 func (list *linkedList) show() {
-	current := list.head
+	// current := list.head		//	current = current.next
+	// or
+	var current = list.head //	current = current.next
+
 	for current != nil {
-		fmt.Println(current.val)
+		fmt.Print(current.val, ">")
 		current = current.next
 	}
 }
 
-func (list *linkedList) ShowFront() (int, error) {
+func (list *linkedList) showFront() error {
 	if list.head == nil {
-		return 0, fmt.Errorf("Cannot Find ShowFront Value in an Empty linked list")
+		return fmt.Errorf("linkedList is empty...")
 	}
-	return list.head.val, nil
+	fmt.Println(list.head.val)
+	return nil
 }
 
-func (list *linkedList) ShowBack() (int, error) {
-	if list.head == nil {
-		return 0, fmt.Errorf("Cannot Find ShowFront Value in an Empty linked list")
+func (list *linkedList) showBack() (int, error) {
+	if list.tail == nil {
+		return -1, fmt.Errorf("linkedList is empty...")
 	}
+	fmt.Println(list.tail.val)
 	return list.tail.val, nil
 }
 
-func (list *linkedList) Reverse() {
-	list.tail = list.head
+func (list *linkedList) reverse() { // list:     *main.linkedList
+	var current = list.head // current:  *main.listNode
+	var previous *listNode  // previous: nil
 
-	current := list.head
-	var prev *listNode
+	var newTail = list.head
 	for current != nil {
-		temp := current.next
-		current.next = prev
-		prev = current
-		current = temp
+		fmt.Print(current.val, ">")
+		var next = current.next
+
+		current.next = previous
+
+		previous = current
+		current = next
 	}
-	list.head = prev
+	list.tail = newTail
+	list.head = previous
 }
 
-func (list *linkedList) Delete(key int) {
-	if list.head.val == key {
-		list.head = list.head.next
-		list.length--
-		return
-	}
+func (list *linkedList) delete(val int) {
+	var current = list.head
+	var previous *listNode
 
-	var prev *listNode = nil
-	curr := list.head
-	for curr != nil && curr.val != key {
-		prev = curr
-		curr = curr.next
+	for current.next != nil {
+		fmt.Print(current.val, " > ")
+		var next = current.next
+
+		if current.val == val {
+			previous.next = next
+			return
+		}
+
+		previous = current
+		current = current.next
 	}
-	if curr == nil {
-		fmt.Println("Key Not found")
-		return
-	}
-	prev.next = curr.next
-	list.length--
-	fmt.Println("Node Deleted")
 
 }
 
 func main() {
-
 	list := linkedList{}
-	//node1 := &listNode{val: 20}
-	//node2 := &listNode{val: 30}
-	//node3 := &listNode{val: 40}
-	//node4 := &listNode{val: 50}
-	//node5 := &listNode{val: 70}
-	//
-	//list.Add(node1)
-	//list.Add(node2)
-	//list.Add(node3)
-	//list.Add(node4)
-	//list.Add(node5)
-
 	list.addNode(10)
-	fmt.Println("Length = ", list.tail.val)
 	list.addNode(20)
-	fmt.Println("Length = ", list.tail.val)
 	list.addNode(30)
-	fmt.Println("Length = ", list.tail.val)
 	list.addNode(40)
-	fmt.Println("Length = ", list.tail.val)
 	list.addNode(50)
 	fmt.Println("Length = ", list.tail.val)
 
 	//list.Show()
 	list.show()
-	fmt.Println("Length = ", list.Len())
+	fmt.Println("Length = ", list.len())
 
-	//list.Delete(40)
-	list.Reverse()
-	//fmt.Println("Length = ", list.Len())
+	fmt.Println("Reverse ")
+
+	list.reverse()
+	fmt.Println("Length = ", list.len())
 	list.show()
-	//
-	//front, _ := list.ShowFront()
-	//back, _ := list.ShowBack()
-	//fmt.Println("ShowFront = ", front)
-	//fmt.Println("ShowBack = ", back)
 
+	fmt.Println("Reverse ")
+	list.reverse()
+	list.show()
+
+	fmt.Println("Delete ")
+	list.delete(30)
+	fmt.Println("Length = ", list.len())
+	list.show()
+
+	fmt.Println("Front: ")
+	list.showFront()
+
+	fmt.Println("Back: ")
+	list.showBack()
 }
