eea11b2ae526ccfd7f3dd0deacaa46365c6d8642
Vijay Jammi <vjammi@gmail.com>
2022-12-27 02:36:51 -0500

    .

--- datastructures/main.go
+++ datastructures/main.go
@@ -2,18 +2,27 @@ package main
 
 import "fmt"
 
-type Queue
+type QueueNode struct {
+	val  any
+	Next *QueueNode
+}
+
+type Queue struct {
+	head   *QueueNode
+	tail   *QueueNode
+	length int
+}
 
 func main() {
 	fmt.Println("Queue Impl in Golang")
 	queue := new(Queue)
-	queue.enqueue("1")
-	queue.enqueue("2")
-	queue.enqueue("3")
-	queue.enqueue("4")
-	queue.enqueue("5")
-	queue.enqueue("6")
-	queue.enqueue("7")
-	queue.enqueue("8")
+	queue.Enqueue("1")
+	//queue.Enqueue("2")
+	// queue.enqueue("3")
+	// queue.enqueue("4")
+	// queue.enqueue("5")
+	// queue.enqueue("6")
+	// queue.enqueue("7")
+	// queue.enqueue("8")
 	fmt.Print(queue)
 }
